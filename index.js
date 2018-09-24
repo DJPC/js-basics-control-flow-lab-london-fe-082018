0b0c42c0dd60678e7f32200b90248258da87809a
Donna Cadiente <djanecad@hotmail.com>
2018-09-24 18:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(distance) {
     else if (distance >2000){
      return "I will gladly take your thirty bucks."
     }
-    else if (distance )
+    else if (distance){
       return "No can do."
       }
   }
